32a8601e13612865d8aa6462e7d38a7192ca17b5
JokerJok <dmehtiev@inbox.ru>
2022-12-16 16:25:42 +0300

    Прописал полностью модель юзера

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,10 +1,24 @@
 const { userController } = require("../controllers/users.controller");
 const { Router } = require("express");
+const { check } = require("express-validator");
+const router = Router();
+// const authMiddleware = require("../middlewares/auth.middleware");
+const roleMiddleware = require("../middlewares/role.middleware");
 
-const router = Router()
-
-router.get('')
-router.post('')
-router.delete('')
+router.get("/users", roleMiddleware, userController.getAllUsers);
+router.post(
+  "/auth",
+  [check("usersName", "Имя пользователя не может быть пустым").notEmpty()],
+  [
+    check(
+      "password",
+      "Пароль должен быть больше 4 символов, но не более 15 символов"
+    ).isLength({ min: 4, max: 15 }),
+  ],
+  userController.registerUser
+);
+router.post("/login", userController.loginUser);
+router.post("/role",  userController.createRoles);
+router.delete("/users/:id", roleMiddleware, userController.deleteUsers);
 
 module.exports = router;
