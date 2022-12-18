03da4898eb9297721acd4c60ce11bd9c4830b4ab
Blacksider1 <adamtsurov123@gmail.com>
2022-12-18 19:05:02 +0300

    Добавил возможность вывода, удаления, изменения отзывов и добавления отзыва к игре

--- models/Games.model.js
+++ models/Games.model.js
@@ -1,16 +1,14 @@
-const mongoose = require("mongoose"); 
-const { SchemaTypes } = require("mongoose"); 
+const mongoose = require('mongoose');
+const { SchemaTypes } = require('mongoose');
 
 const gamesModel = mongoose.Schema({
   images: [],
   name: String,
   description: String,
-  reviews: {
-    type: SchemaTypes.Array
-  },
-  publisher: String
-}) 
+  reviews: [{ type: SchemaTypes.ObjectId, ref: 'Reviews' }],
+  publisher: String,
+});
 
-const Games = mongoose.model('Games', gamesModel)
+const Games = mongoose.model('Games', gamesModel);
 
-module.exports = Games
+module.exports = Games;
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = Router()
 
 router.use(require('./users.route'))
 router.use(require('./games.route'))
+router.use(require('./reviews.route'))
 
 module.exports = router
